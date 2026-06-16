12f217fe17b56ad2f0df53ff5605210906d22bc8
Neha27-G <niagupta8@gmail.com>
2026-06-17 01:36:10 +0530

    programs on pattern printing

---
+++ Assignments/Assignment_27/program01.c
@@ -0,0 +1,54 @@
+/*
+Accept number of rows and number of columns from user and display bellow pattern
+
+Input : iRow = 4    iCol = 4
+
+Output :
+          *  
+          *  *   
+          *  *   *   
+          *  *   *   *
+         
+ */
+
+ #include<stdio.h>
+
+ void Pattern(int iRow , int iCol)
+ {
+    int i= 0, j = 0;
+
+     if(iRow != iCol)
+    {
+        printf("Inalid Parameters");
+        printf("Number of Rows and Columns should be Same");
+        return;
+    }
+
+    for(i = 1; i <= iRow; i++)
+    {  
+        for(j = 1; j <= i; j++)
+        {
+            if(i >= j)
+            {
+                printf("*\t");
+            }
+           
+        }
+        printf("\n");
+    }
+ }
+
+ int main()
+ {
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the Number of Rows:");
+    scanf("%d",&iValue1);
+
+    printf("Enter the Number of Column:");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+ }
---
+++ Assignments/Assignment_27/program05.c
@@ -0,0 +1,59 @@
+/*
+Accept number of rows and number of columns from user and display bellow pattern
+
+Input : iRow = 4   iCol = 4
+
+Output :
+            1   2   3   4
+                2   3   4
+                    3   4
+                        4
+          
+          
+         
+ */
+
+ #include<stdio.h>
+
+ void Pattern(int iRow , int iCol)
+ {
+    int i= 0, j = 0;
+
+     if(iRow != iCol)
+    {
+        printf("Inalid Parameters");
+        printf("Number of Rows and Columns should be Same");
+        return;
+    }
+
+    for(i = 1; i <= iRow; i++)
+    {  
+        for(j = 1; j <= iCol; j++)
+        {
+            if( j < i)
+            {
+                printf(" \t");
+            }
+            else
+            {
+                printf("%d\t",j);
+            }    
+        }
+        printf("\n");
+    }
+ }
+
+ int main()
+ {
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the Number of Rows:");
+    scanf("%d",&iValue1);
+
+    printf("Enter the Number of Column:");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+ }
