04f5f4ad76ff01e39ec004d9db819734d5095ea2
Neha27-G <niagupta8@gmail.com>
2026-06-29 22:33:21 +0530

    program performing Iteration

--- Application Programs/program23.c
+++ Application Programs/program23.c
@@ -3,14 +3,14 @@
 void Display(int iNo)
 {
     int iCnt = 0;
-    
+
     //Updator
     if(iNo < 0)
     {
         iNo = -iNo;
     }
 
-    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    for(iCnt = iNo; iCnt >= 1; iCnt--)
     {
         printf("Jay Ganesh...\n");
     }
