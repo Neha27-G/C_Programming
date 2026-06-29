d4a59b160df78c1d171dd83bc2e764e9705a5f3d
Neha27-G <niagupta8@gmail.com>
2026-06-29 21:20:08 +0530

    program performing Iteration

---
+++ Application Programs/program20.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0, iValue = 0;
+
+    printf("Enter the Frequency :");
+    scanf("%d",&iValue);
+
+    for(iCnt = 1; iCnt <= iValue; iCnt++)
+    {
+        printf("Jay Ganesh...\n");
+    }
+    
+    return 0;
+}
