8e91607f91d208914d6d5247a9621f2d5ebae1f0
Neha27-G <niagupta8@gmail.com>
2026-07-05 00:54:25 +0530

    program to check perfect Number

---
+++ Application Programs/program37.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int  SumFactors(int iNo)
+{
+    int iCnt = 0;
+    int iSum = 0;
+
+    for(iCnt = 1; iCnt <= (iNo/2); iCnt++)        //reduce time complexity
+    {
+        if((iNo % iCnt)==0)
+        {
+            iSum = iSum + iCnt;
+        }
+    }
+    return iSum;
+    
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter the Number :");
+    scanf("%d",&iValue);
+
+    iRet = SumFactors(iValue);
+
+    printf("Summation of Factors : %d\n",iRet);
+
+    return 0;
+}
