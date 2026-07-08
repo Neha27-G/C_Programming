dd992ebee28a67d51d57e9e075ce83aa0b488089
Neha27-G <niagupta8@gmail.com>
2026-07-08 23:21:15 +0530

    program to check prime number or not (bFlag)

---
+++ Application Programs/program50.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckPrime(int iNo)
+{
+    int iCnt = 0;
+    bool bFlag = false;                //default value of boolean is false
+
+    for(bFlag = true ,iCnt = 2; iCnt <= (iNo/2); iCnt++)
+    {
+        if((iNo % iCnt)== 0)
+        {
+            bFlag = false; 
+            break;                
+        }                                 
+    }
+    return bFlag;
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter the Number :");
+    scanf("%d",&iValue);
+
+    bRet = CheckPrime(iValue);
+
+    if(bRet == true)
+    {
+        printf("It is Prime Number\n");
+    }
+    else
+    {
+        printf("It is not Prime Number\n");
+    }
+
+    return 0;
+}
