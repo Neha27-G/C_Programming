dd905ff33e6e37f60e87bbeff0f692dd8ba59d80
Neha27-G <niagupta8@gmail.com>
2026-07-08 23:19:25 +0530

    program to check prime number or not

---
+++ Application Programs/program47.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckPrime(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 2; iCnt <= iNo/2; iCnt++)
+    {
+        if((iNo % iCnt)== 0)
+        {
+            break;
+        }
+    }
+
+    if(iCnt < iNo/2)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter the Number :");
+    scanf("%d",&iValue);
+
+    bRet = CheckPrime(iValue);
+
+    if(bRet == true)
+    {
+        printf("It is Prime Number\n");
+    }
+    else
+    {
+        printf("It is not Prime Number\n");
+    }
+
+    return 0;
+}
