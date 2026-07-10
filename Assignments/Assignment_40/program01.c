30cb057c1a5177b153695b00710982e745a68ac0
Neha27-G <niagupta8@gmail.com>
2026-07-10 23:46:09 +0530

    Programs on Singly Linked List

---
+++ Assignments/Assignment_40/program01.c
@@ -0,0 +1,86 @@
+//Consider Singly Linear LinkedList to solve problems
+// 1. Display Only Odd Elements..   print only Old elements.
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void Display(PNODE first)
+{
+    while (first != NULL)                                            //type 1                                           
+    {
+        printf(" | %d | -> ",first->data);
+        first = first->next;                            
+    }
+    printf("NULL\n");
+}
+
+void InsertLast(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+    
+    newn->data = iNo;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        temp = *first;
+
+        while(temp->next != NULL)
+        {
+            temp = temp-> next;
+        }
+        temp->next = newn;
+    }
+}
+
+void DisplayOdd(PNODE First)
+{
+    while(First != NULL)
+    {
+        if((First->data % 2) != 0)
+        {
+            printf("%d\t",First->data);
+        }
+
+        First = First ->next;
+    }
+  
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+
+
+    InsertLast(&head,11);
+    InsertLast(&head,20);
+    InsertLast(&head,51);
+    InsertLast(&head,100);
+    InsertLast(&head,111);
+
+    Display(head);
+
+    printf("Odd Elements are : ");
+    DisplayOdd(head);
+
+    return 0;
+}
