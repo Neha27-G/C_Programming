6faa38c932bd8ff9eaa082c7114335d89d534a4e
Neha27-G <niagupta8@gmail.com>
2026-07-18 00:38:52 +0530

    Program to Sum of Digits of the Number

---
+++ Application Programs/program70.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int SumDigits(int iNo)
+{
+    int iDigit = 0;
+    int iSum = 0;
+
+    while(iNo != 0)
+    {
+        iSum = iSum + iNo % 10;
+        iNo= iNo / 10;
+    }
+    return iSum;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter the Number : ");
+    scanf("%d",&iValue);
+
+    iRet = SumDigits(iValue);
+    printf("Summation of the Digits are : %d",iRet);
+ 
+    return 0;
+}
