2a8025b4fc555de96c02a2a427429ee1944135a0
Neha27-G <niagupta8@gmail.com>
2026-07-17 23:58:06 +0530

    program to Count Digits of the Number

---
+++ Application Programs/program62.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int CountDigits(int iNo)
+{
+    int iDigit = 0;
+    int iCount = 0;
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        iCount++;
+        iNo= iNo / 10;
+    }
+    return iCount;
+}
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter the Number : ");
+    scanf("%d",&iValue);
+
+    iRet = CountDigits(iValue);
+    printf("Number of Digits are : %d",iRet);
+    
+    return 0;
+}
